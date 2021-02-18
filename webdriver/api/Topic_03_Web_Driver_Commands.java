f70174880f06675d0bbc1d326a4bf033c43efe62
Trieu Thi Hoa <trieuthihoa97@gmail.com>
2021-02-18 14:41:51 +0700

    Add new class

---
+++ webdriver/api/Topic_03_Web_Driver_Commands.java
@@ -0,0 +1,44 @@
+package api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_03_Web_Driver_Commands {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
