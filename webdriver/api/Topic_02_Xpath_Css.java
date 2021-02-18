aaf20fdf950cb4b772559ad651fca3914d705dc3
Trieu Thi Hoa <trieuthihoa97@gmail.com>
2021-02-18 13:55:33 +0700

    Add Topic 02 (Xpath/Css)

--- webdriver/api/Topic_02_Xpath_Css.java
+++ webdriver/api/Topic_02_Xpath_Css.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+public class Topic_02_Xpath_Css {
 	WebDriver driver;
 
 	@BeforeClass
